a3de8fb1094c3c981c0d172117083e3f2a0ef4af
takjakim <takjakim.apple@gmail.com>
2019-10-19 21:01:20 +0900

    no message

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "ㅅㄷㄴㅅ",
+        "excerpt":"ㅅㄷㄴㅁㅅㄴㄷㅅ  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/%ED%85%8C%EC%8A%A4%ED%8A%B8/",
+        "teaser":null}]
