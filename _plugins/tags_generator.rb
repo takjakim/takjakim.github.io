81e8383b3467677346226f1c9530d7f89d8feb97
takjakim <56712777+takjakim@users.noreply.github.com>
2026-06-23 09:43:33 +0900

    fix: stabilize tag page date sorting

--- _plugins/tags_generator.rb
+++ _plugins/tags_generator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'time'
+
 # Generate /tags/<tag>/ pages for the notes collection.
 
 module Jekyll
@@ -27,6 +29,14 @@ module Jekyll
   end
 
   class TagPage < Page
+    def sort_timestamp(doc)
+      value = doc.data['last_modified_at'] || doc.data['date'] || Time.at(0)
+      return value.to_time.to_i if value.respond_to?(:to_time)
+      return Time.parse(value.to_s).to_i
+    rescue StandardError
+      0
+    end
+
     def initialize(site, tag, docs)
       super(site, site.source, File.join('tags', Utils.slugify(tag)), 'index.html')
       self.data['layout'] = 'tag'
@@ -38,7 +48,7 @@ module Jekyll
       self.data['last_modified_at'] = now
       self.data['last_modified_at_timestamp'] = now.to_i
 
-      self.data['items'] = docs.sort_by { |d| d.data['last_modified_at'] || d.data['date'] || Time.at(0) }.reverse.map do |d|
+      self.data['items'] = docs.sort_by { |d| sort_timestamp(d) }.reverse.map do |d|
         {
           'title' => d.data['title'],
           'url' => d.url,
